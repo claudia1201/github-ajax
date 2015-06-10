92831fa56dec90b4027b0702dd9546c308358c57
Claudia Calzadillas <cdat2011@hotmail.com>
2015-06-10 13:43:37 -0400

    Secon Commit, Uploded HTML, CSS and JS

---
+++ public/ajax.js
@@ -0,0 +1,68 @@
+$(document).on("ready", function(){ 
+ 
+  $.ajax({
+    url: "https://api.github.com/users/claudia1201",
+    method: "GET",
+    data: {
+      access_token: "8bae437d64a874e34746afc526c87c9b1de3507f"
+    },
+    success: function(data) { 
+    	var source = $("#leftSideUser").html()
+
+    	var templateFunction = Handlebars.compile(source)
+
+    	var htmlString = templateFunction({
+        avatar_url: data.avatar_url,
+        name: data.name,
+        login: data.login,
+        company: data.company,
+        location: data.location,
+        email: data.email,
+        created_at: moment(data.created_at).format("MMMM, DD, YYYY")
+      })
+
+    	$(".userBox").append(htmlString)
+
+     }
+  })
+
+  $.ajax({
+    url: "https://api.github.com/users/claudia1201/repos",
+    method: "GET",
+    data: {
+    access_token: "8bae437d64a874e34746afc526c87c9b1de3507f"
+    },
+    success: function(data) {
+
+      _.each(data, function(data){
+
+        $.ajax({
+          url: data.url,
+          method: "GET",
+          data: {
+          access_token: "8bae437d64a874e34746afc526c87c9b1de3507f"
+          },
+          success: function(repo) {
+
+            var source = $("#rightSideUser").html()
+
+            var templateFunction = Handlebars.compile(source)
+
+            var htmlString = templateFunction({
+              name: data.name,
+              language: data.language,
+              stargazers_count: repo.stargazers_count,
+              network_count: repo.network_count,
+              description: data.description,
+              updated_at:  moment(data.updated_at).fromNow()
+            })
+
+            $(".repoBox").append(htmlString)
+          }
+        })
+      })
+    }
+
+  })
+ 
+})
